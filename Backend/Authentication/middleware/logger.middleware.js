3fb40e8e8a5d9e09ed5a6b355abbbdbe2eb12e77
mdharoonhussain <haroonhussain97@gmail.com>
2023-05-08 18:12:59 +0530

    done

--- Backend/Authentication/middleware/logger.middleware.js
+++
@@ -1,9 +0,0 @@
-
-const logger = (req, res, next) => {
-    console.log(`Method: ${req.method}  | Route: ${req.url}`)
-    next()
-  
-}
-module.exports = {
-    logger
-}
--- Backend/Authentication/model/test.js
+++
@@ -1,9 +0,0 @@
-const mongoose=require("mongoose")
-const testSchema = mongoose.Schema({
-    email:String,
-    name:String,
-    check:String
-})
-const testModel = mongoose.model("test",testSchema)
-
-module.exports={testModel} 
