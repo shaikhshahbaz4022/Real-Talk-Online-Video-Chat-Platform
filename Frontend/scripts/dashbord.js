5b355dffef4851b29b7024b46aa0ed9cd0c8e6aa
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-05-09 04:17:39 +0530

    done

--- Frontend/scripts/dashbord.js
+++ Frontend/scripts/dashbord.js
@@ -24,3 +24,23 @@ centerVideoBtn.addEventListener("click",()=>{
 centerChatBtn.addEventListener("click",()=>{
     window.location.href="./room.html?type=message"
 })
+
+
+//data append from local storage---------------->
+let token = localStorage.getItem("token");
+const userinfo = JSON.parse(localStorage.getItem("userDetails"))||null;
+console.log(userinfo);
+console.log(token)
+
+
+
+//tags----------->
+const name1 = document.getElementById("append-name");
+const name2 = document.getElementById("welcome-name");
+const email = document.getElementById("append-email");
+
+if(userinfo){
+    name1.innerText = userinfo.name;
+    name2.innerText = userinfo.name;
+    email.innerText = userinfo.email;
+}
